1dd9ae4e25bb0a83db4f38cf9051f20e89d53b64
ShahbazKhan2003 <shahbazkhan082003@gmail.com>
2025-01-24 12:29:26 +0530

    lombok correction in pom

--- src/main/java/com/example/ems/entity/Employee.java
+++ src/main/java/com/example/ems/entity/Employee.java
@@ -1,8 +1,15 @@
 package com.example.ems.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "employees")
 public class Employee {
@@ -15,46 +22,4 @@ public class Employee {
     private String lastName;
     @Column(name = "email_id", nullable = false, unique = true)
     private String email;
-
-    public Employee(Long id, String firstName, String lastName, String email) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-    }
-    public Employee(){
-
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
